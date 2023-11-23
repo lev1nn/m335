29d0935d303cef484f83ac040c5cc2f8891f3eb7
lev1nn <levin.schaller@bbzbl-it.ch>
2023-11-23 14:56:05 +0100

    Update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://mogduyqpizugbhzzkfso.supabase.co',
+  supabaseClient:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1vZ2R1eXFwaXp1Z2JoenprZnNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxNDU4MzMsImV4cCI6MjAxNTcyMTgzM30.5MpfApmFoQSL36frJUD8xCxtyliSUp5l8n1QNzgQosU',
 };
