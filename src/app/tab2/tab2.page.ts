6bec2759992221a94f3e7592a3bb8337a9239d9d
lev1nn <levin.schaller@bbzbl-it.ch>
2023-11-23 10:52:04 +0100

    Add library view

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,55 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { SupabaseClient, createClient } from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment';
+import { Photo } from '../models/photo.model';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    ExploreContainerComponent,
+    CommonModule,
+  ],
 })
 export class Tab2Page {
+  private client: SupabaseClient;
+  public images: Photo[] = [];
 
-  constructor() {}
+  constructor(private router: Router) {
+    this.client = createClient(
+      environment.supabaseUrl,
+      environment.supabaseClient
+    );
 
+    this.fetchImages();
+  }
+
+  ionViewWillEnter() {
+    this.fetchImages();
+  }
+
+  async fetchImages() {
+    let { data: images, error } = await this.client.from('images').select('*');
+
+    if (error) {
+      console.error('Error fetching images:', error);
+    } else {
+      this.images = images as [Photo];
+    }
+  }
 }
